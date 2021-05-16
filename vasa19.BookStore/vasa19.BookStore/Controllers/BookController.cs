2e56186e12535297070c9f19eb5ad1069634ae1d
Vasu Saxena <vasusaxena19@gmail.com>
2021-05-16 20:23:27 +0530

    Learned Controller

---
+++ vasa19.BookStore/vasa19.BookStore/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace vasa19.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+
+        public string GetBook(int id)
+        {
+            return $"Book with Id = {id}";
+        }
+
+        public string SearchBooks(string bookName, string authorName)
+        {
+            return $"Book with name = {bookName} & Author = {authorName}";
+        }
+    }
+}
