16f951b1f9c5bd4915f23b6668c8250bd75f5cee
Vasu Saxena <vasusaxena19@gmail.com>
2021-05-19 16:53:05 +0530

    Basic Dynamic Website

--- vasa19.BookStore/vasa19.BookStore/Controllers/BookController.cs
+++ vasa19.BookStore/vasa19.BookStore/Controllers/BookController.cs
@@ -20,12 +20,13 @@ namespace vasa19.BookStore.Controllers
         public ViewResult GetAllBooks()
         {
             var data =  _bookRepository.GetAllBooks();
-            return View();
+            return View(data);
         }
 
-        public BookModel GetBook(int id)
+        public ViewResult GetBook(int id)
         {
-            return _bookRepository.GetBookById(id);
+            var data =  _bookRepository.GetBookById(id);
+            return View(data);
         }
 
         public List<BookModel> SearchBooks(string bookName, string authorName)
--- vasa19.BookStore/vasa19.BookStore/Repository/BookRepository.cs
+++ vasa19.BookStore/vasa19.BookStore/Repository/BookRepository.cs
@@ -27,11 +27,12 @@ namespace vasa19.BookStore.Repository
         {
             return new List<BookModel>()
             {
-                new BookModel(){Id = 1, Title = "MVC", Author = "Nitish"},
-                new BookModel(){Id = 2, Title = "Python", Author = "Vasu"},
-                new BookModel(){Id = 3, Title = "C#", Author = "Monika"},
-                new BookModel(){Id = 4, Title = "Java", Author = "Jacob"},
-                new BookModel(){Id = 5, Title = "Php", Author = "Alex"},
+                new BookModel(){Id = 1, Title = "MVC", Author = "Nitish", Description = "This is the description for MVC book", Category = "Framework",Language = "English", TotalPages = 134},
+                new BookModel(){Id = 2, Title = "Python", Author = "Vasu", Description = "This is the description for Python book", Category = "Programming",Language = "English", TotalPages = 567},
+                new BookModel(){Id = 3, Title = "C#", Author = "Monika", Description = "This is the description for C# book", Category = "Developer",Language = "Chinese", TotalPages = 434},
+                new BookModel(){Id = 4, Title = "Java", Author = "Jacob", Description = "This is the description for Java book", Category = "Concept",Language = "French", TotalPages = 998},
+                new BookModel(){Id = 5, Title = "Php", Author = "Alex", Description = "This is the description for Php book", Category = "Programming",Language = "Hindi", TotalPages = 234},
+                new BookModel(){Id = 6, Title = "Azure DevOps", Author = "Kartik", Description = "This is the description for Azure DevOps book", Category = "DevOps",Language = "English", TotalPages = 614},
             };
         }
     }
